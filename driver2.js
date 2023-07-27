a3902645b074b696982327b57064b0f11294a33e
adifahmad <khaironi20@gmail.com>
2023-07-28 05:21:41 +0700

    progress challenge2

--- driver2.js
+++ driver2.js
@@ -1,7 +1,8 @@
 function deretKaskus(n) {
   let kelLima = [];
   for (let i = 0; i < n; i++) {
-    kelLima.push(3 * (i + 1));
+    let kalkulasi = 3 * (i + 1);
+    kelLima.push(kalkulasi);
     if (kelLima[i] % 5 === 0 && kelLima[i] % 6 === 0) {
       kelLima[i] = "KASKUS";
     } else if (kelLima[i] % 5 === 0) {
