e938b02d6c75479f7b8b8c2ec3115db0506bd170
adifahmad <khaironi20@gmail.com>
2023-08-01 14:36:04 +0700

    Progress Challenge 6

--- driver6.js
+++ driver6.js
@@ -1,15 +1,21 @@
 function sentencesManipulation(sentence) {
-  let output = "";
-  let vocal = "aiueo";
-  let result = sentence.split(" ");
+  let result = [];
+  let output = [];
+  result = sentence.split(" ");
   for (let i = 0; i < result.length; i++) {
-    if (vocal.includes(result[i].charAt(0)) == true) {
-      output += result[i] + " ";
-    } else if (vocal.includes(result[i].charAt(0)) == false) {
-      output += result[i].slice(1) + result[i].charAt(0) + "nyo" + " ";
+    if (
+      result[i][0] === "a" ||
+      result[i][0] === "i" ||
+      result[i][0] === "u" ||
+      result[i][0] === "e" ||
+      result[i][0] === "o"
+    ) {
+      output.push(result[i]);
+    } else {
+      output.push(result[i].slice(1) + result[i][0] + "nyo");
     }
   }
-  console.log(output);
+  console.log(output.join(" "));
 }
 
 sentencesManipulation("ibu pergi ke pasar bersama aku");
